7dc2b88422d2c85af67ad1eb9cd6dd309e33b165
shivba28 <72241390+shivba28@users.noreply.github.com>
2024-09-09 16:21:36 -0700

    added logic for tag visibility

--- src/components/consoleText.js
+++ src/components/consoleText.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import '../assets/CSS/text.css';
 
 
@@ -9,6 +9,8 @@ export const ConsoleText = ({ words, colors }) => {
   const [waiting, setWaiting] = useState(false);
   const [currentWordIndex, setCurrentWordIndex] = useState(0);
   const [currentColorIndex, setCurrentColorIndex] = useState(0);
+  const [isInView, setIsInView] = useState(false);
+  const textRef = useRef(null);
 
   useEffect(() => {
     if (!colors || colors.length === 0) {
@@ -16,41 +18,62 @@ export const ConsoleText = ({ words, colors }) => {
     }
 
     const intervalId = setInterval(() => {
-      if (letterCount === 0 && !waiting) {
-        setWaiting(true);
-        setTimeout(() => {
-          setCurrentColorIndex((currentColorIndex + 1) % colors.length);
-          setCurrentWordIndex((currentWordIndex + 1) % words.length);
-          setX(1);
-          setLetterCount(1);
-          setWaiting(false);
-        }, 1000);
-      } else if (letterCount === words[currentWordIndex].length + 1 && !waiting) {
-        setWaiting(true);
-        setTimeout(() => {
-          setX(-1);
-          setLetterCount((prev) => prev + x);
-          setWaiting(false);
-        }, 1000);
-      } else if (!waiting) {
-        setLetterCount((prev) => prev + x);
-      }
+        if(isInView){
+            if (letterCount === 0 && !waiting) {
+                setWaiting(true);
+                setTimeout(() => {
+                setCurrentColorIndex((currentColorIndex + 1) % colors.length);
+                setCurrentWordIndex((currentWordIndex + 1) % words.length);
+                setX(1);
+                setLetterCount(1);
+                setWaiting(false);
+                }, 1000);
+            } else if (letterCount === words[currentWordIndex].length + 1 && !waiting) {
+                setWaiting(true);
+                setTimeout(() => {
+                setX(-1);
+                setLetterCount((prev) => prev + x);
+                setWaiting(false);
+                }, 1000);
+            } else if (!waiting) {
+                setLetterCount((prev) => prev + x);
+            }
+        }
     }, 120);
 
     return () => clearInterval(intervalId); // Clean up on unmount
-  }, [letterCount, waiting, currentWordIndex, x, colors, words]);
+  }, [letterCount, waiting, currentWordIndex, x, colors, words, isInView]);
 
   useEffect(() => {
     const underscoreIntervalId = setInterval(() => {
-      setVisible((prevVisible) => !prevVisible);
+        if (isInView) {
+            setVisible((prevVisible) => !prevVisible);
+          }
     }, 400);
 
     return () => clearInterval(underscoreIntervalId); // Clean up on unmount
+  }, [isInView]);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        setIsInView(entry.isIntersecting);
+      });
+    });
+
+    if (textRef.current) {
+      observer.observe(textRef.current);
+    }
+
+    return () => {
+      if (textRef.current) {
+        observer.unobserve(textRef.current);
+      }
+    };
   }, []);
 
   return (
-    <div class='console-container'>
-    
+    <div class='console-container' ref={textRef}>
       <h1 id="text" style={{ color: colors[currentColorIndex], fontStyle:"normal", fontSize: '5rem', fontWeight:"900" }}>
         {words[currentWordIndex].substring(0, letterCount)}
       </h1>

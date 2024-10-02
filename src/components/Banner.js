f3816f4a9884508688cb366365f50142a72c5593
shivba28 <shivba28@csu.fullerton.edu>
2024-10-01 19:25:38 -0700

    gsap for banner kinda working. Need to configure

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,59 +1,84 @@
 import React, { useState, useEffect, useRef } from 'react';
 import video from "../assets/videos/intro.mp4";
+import gsap from 'gsap';
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useGSAP } from "@gsap/react";
 //import { ConsoleText } from '../components/Text-Animations/consoleText'
 import { TextScramble } from '../components/Text-Animations/scrambleText';
 
 export const Banner = () => {
     
-
-    const [divHeight, setDivHeight] = useState((() => {if(window.innerWidth < 873){return 225;} else{return 340;}}));
-    const [divWidth, setDivWidth] = useState((() => {if(window.innerWidth < 873){return 300;} else{return 800;}}));
-
-    
+    gsap.registerPlugin(ScrollTrigger);
 
     const videoRef = useRef(null);
 
+    useGSAP(() => {
+        gsap.timeline({
+            scrollTrigger: {
+                trigger: '.home',
+                start: 'top',
+                end: "+=40%",
+                scrub: true,
+                markers: true,
+            }
+        })
+        .to(".video-container video", {
+            scale: 2.1,
+            z: 150,
+            transformOrigin: "center center",
+            ease: "power1.inOut"
+          },
+        );
+    })
 
-  const handleScroll = () => {
-    const scrollTop = window.scrollY;
-    const winHeight = window.innerHeight;
-    const winWidth = window.innerWidth;
-    const docHeight = document.body.scrollHeight - winHeight;
-    const scrollFraction = (scrollTop) / (docHeight * 0.4) ;
+    // const [divHeight, setDivHeight] = useState((() => {if(window.innerWidth < 873){return 225;} else{return 340;}}));
+    // const [divWidth, setDivWidth] = useState((() => {if(window.innerWidth < 873){return 300;} else{return 800;}}));
+
+    
+
+    
+
+
+//   const handleScroll = () => {
+//     const scrollTop = window.scrollY;
+//     const winHeight = window.innerHeight;
+//     const winWidth = window.innerWidth;
+//     const docHeight = document.body.scrollHeight - winHeight;
+//     const scrollFraction = (scrollTop) / (docHeight * 0.4) ;
 
         
     
 
-    if(winWidth < 873) {
-        setDivHeight(225);
-        setDivWidth(300 + scrollFraction * winWidth)
-        const newWidth = 300 + scrollFraction * winWidth;
-        setDivWidth(Math.min(newWidth,winWidth + 30));
-    } else{
-        setDivHeight(340);
-        setDivWidth(800 + scrollFraction * winWidth);
-        const newWidth = 800 + scrollFraction * winWidth;
-        setDivWidth(Math.min(newWidth,winWidth));
-    }
+//     if(winWidth < 873) {
+//         setDivHeight(225);
+//         setDivWidth(300 + scrollFraction * winWidth)
+//         const newWidth = 300 + scrollFraction * winWidth;
+//         setDivWidth(Math.min(newWidth,winWidth + 30));
+//     } else{
+//         setDivHeight(340);
+//         setDivWidth(800 + scrollFraction * winWidth);
+//         const newWidth = 800 + scrollFraction * winWidth;
+//         setDivWidth(Math.min(newWidth,winWidth));
+//     }
 
-    const videoDiv = document.querySelector('.video-container');
-    const aboutSection = document.querySelector('.about-section');
+//     const videoDiv = document.querySelector('.video-container');
+//     const aboutSection = document.querySelector('.about-section');
 
-    if (scrollTop > winHeight/2) {
-        videoDiv.classList.add('sticky');
-        aboutSection.classList.add('show');
-      } else {
-        videoDiv.classList.remove('sticky');
-        aboutSection.classList.remove('show');
-      }
+//     if (scrollTop > winHeight/2) {
+//         videoDiv.classList.add('sticky');
+//         aboutSection.classList.add('show');
+//       } else {
+//         videoDiv.classList.remove('sticky');
+//         aboutSection.classList.remove('show');
+//       }
 
-  };
+//   };
 
-  useEffect(() => {
-    window.scrollTo(0, 0); // Scroll to the top on page load
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
+//   useEffect(() => {
+//     window.scrollTo(0, 0); // Scroll to the top on page load
+//     window.addEventListener("scroll", handleScroll);
+//     return () => window.removeEventListener("scroll", handleScroll);
+//   }, []);
 
   useEffect(() => {
     const videoElement = videoRef.current;
@@ -79,7 +104,6 @@ export const Banner = () => {
                 <div className="slogan-left"><h1 className="left">THIS IS</h1></div>
                 <div className="slogan-right"><h1 className="right">my story</h1></div>
             </div>
-
             {/* 
             <iframe
                 width={560} height={316} 
@@ -91,14 +115,14 @@ export const Banner = () => {
                 allowfullscreen></iframe>
             </div>
             )} */}
-
-            <div className={`video-container`} style={{ height: `${divHeight}px`, width: `${divWidth}px`}}>
+{/* style={{ height: `${divHeight}px`, width: `${divWidth}px`}} */}
+            <div className={`video-container`} >
                 <video
                     ref={videoRef} 
                     loop 
                     muted 
                     playsInline
-                >
+                 className='video'>
                     <source src={video} type="video/mp4" />
                     Your browser does not support the video tag.
                 </video>

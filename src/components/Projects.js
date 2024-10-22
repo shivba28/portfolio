b02d5ac26a7404f3284b158b37221182337e3dd7
shivba28 <shivba28@csu.fullerton.edu>
2024-10-21 22:03:10 -0700

    added more project assets. Need to redo project title

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,6 +9,9 @@ import { Row, Col } from "react-bootstrap";
 import constructionData from '../assets/Lottie/CCDP.json';
 import school from '../assets/Lottie/trimmed_school.json';
 import pacman from '../assets/videos/pacman.mp4';
+import ans from '../assets/Lottie/ANS.json';
+import valentines from '../assets/Lottie/Valentine.json';
+import chatbot from '../assets/Lottie/Chatbot.json';
 
 export const Project = () => {
 
@@ -63,8 +66,8 @@ export const Project = () => {
                 {/* <section className="section hero"></section> */}
                 <section className="section" id="project">
                     <div className="project-items">
-                        <Row className="align-items-center">
-                            <Col xs={12} md={6} xl={4}>
+                        <Row className="my-5 vh-40">
+                            <Col xs={12} sm={8} md={6} xl={4} className="m-auto">
                                 <div className="project-item" id="project-1">
                                     <div className="project-item-active"></div>
                                     <div className="base"></div>
@@ -74,9 +77,9 @@ export const Project = () => {
                                     <div className="isometric-button noselect">DURATION PREDICTOR</div>
                                 </div>
                             </Col>
-                            <Col xs={12} md={6} xl={4}>
+                            <Col xs={12}sm={8} md={6} xl={4} className="m-auto">
                                 <div className="project-item" id="project-2">
-                                <div className="project-item-active"></div>
+                                    <div className="project-item-active"></div>
                                     <div className="base"></div>
                                     <div className="lottie-figure">
                                         <Lottie animationData = {school} loop = {true} autoplay = {true}  />
@@ -84,7 +87,7 @@ export const Project = () => {
                                     <div className="isometric-button noselect">School Website</div>
                                 </div>
                             </Col>
-                            <Col xs={12} md={6} xl={4}>
+                            <Col xs={12} sm={8} md={6} xl={4} className="m-auto">
                                 <div className="project-item" id="project-3">
                                     <div className="project-item-active"></div>
                                     <div className="base"></div>
@@ -98,7 +101,39 @@ export const Project = () => {
                                     <div className="isometric-button noselect">Pacman-3D</div>
                                 </div>
                             </Col>
-                        </Row>  
+                        </Row> 
+                        <Row className="my-5 vh-40">
+                            <Col xs={12} sm={8} md={6} xl={4} className="m-auto">
+                                <div className="project-item" id="project-4">
+                                    <div className="project-item-active"></div>
+                                    <div className="base"></div>
+                                    <div className="lottie-figure">
+                                        <Lottie animationData = {ans} loop = {true} autoplay = {true}  />
+                                    </div>
+                                    <div className="isometric-button noselect">Adopt Not Shop</div>
+                                </div>
+                            </Col>
+                            <Col xs={12} sm={8} md={6} xl={4} className="m-auto">
+                                <div className="project-item" id="project-5">
+                                    <div className="project-item-active"></div>
+                                    <div className="base"></div>
+                                    <div className="lottie-figure">
+                                        <Lottie animationData = {valentines} loop = {true} autoplay = {true}  />
+                                    </div>
+                                    <div className="isometric-button noselect">Valentine's Game</div>
+                                </div>
+                            </Col>
+                            <Col xs={12} sm={8} md={6} xl={4} className="m-auto">
+                                <div className="project-item" id="project-6">
+                                    <div className="project-item-active"></div>
+                                    <div className="base"></div>
+                                    <div className="lottie-figure">
+                                        <Lottie animationData = {chatbot} loop = {true} autoplay = {true}  />
+                                    </div>
+                                    <div className="isometric-button noselect">Chat-Bot</div>
+                                </div>
+                            </Col>
+                        </Row>
                     </div>
                 </section>
             </div>

db7e311e50dedb9b4aac9eeac84fa080845d5c35
shivba28 <shivba28@csu.fullerton.edu>
2024-10-27 10:22:36 -0700

    About fix

--- src/components/About.js
+++ src/components/About.js
@@ -24,9 +24,8 @@ export const About = ({ show, handleClose }) => {
                             Hey there! I'm <b style={{color: 'aquamarine', fontStyle:'italic'}}>Shivba</b>, a software developer by day and a bug-squasher by night.
 I spend my time wrestling with code, upgrading websites, and making sure your apps load faster than your morning coffee — because trust me, I drink a lot of coffee.<br/><br/>
 I’ve successfully migrated many large scale web apps, which makes me wonder why my own website still takes forever to load.<br/><br/>
-<span>I’m fluent in Python, JavaScript, C#, and a bunch of other techie stuff that makes websites work smoothly (most of the time).
+<span>I’m fluent in Python, JavaScript, C#, and a bunch of other techie stuff that makes websites work smoothly (most of the time). I’ve even dabbled in making APIs, and yes, it’s as nerdy as it sounds.<br/><br/>
 {/*I’ve worked with everything from React to SQL, and if there’s a tool out there to make things faster or better, I’ll figure out how to use it.*/}
-I’ve even dabbled in making APIs, and yes, it’s as nerdy as it sounds.<br/><br/>
 {/*Fueled by caffeine and curiosity, when I'm not buried in code, you’ll find me dreaming up ways to reduce hosting costs or teaching my digital pen pal to crack better jokes (thanks, OpenAI).<br/><br/> */}
 In short: I build things, break things (occasionally), and try to make the web a better place, one API call (and coffee cup) at a time!</span>
                             </p>

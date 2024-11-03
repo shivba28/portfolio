c25bab51bb01788c6c14a50c71f59f226b514d8e
shivba28 <shivba28@csu.fullerton.edu>
2024-11-03 00:09:41 -0700

    form styling complete

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -51,7 +51,25 @@ export const Contact = () => {
                         <label for="message" class="form__label">Message</label>
                         <textarea data-cursor="text" className='form__field' name="message" placeholder='Message' id='message' />
                       </div>
-                        <input data-cursor="block" className='bg-secondary bg-gradient mt-5 btn' type="submit" value="Send" />
+                      <div className='d-flex flex-row flex-wrap'>
+                        <button data-cursor="block" className='mt-5 me-auto' type="submit">
+                          <span>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-send" viewBox="0 0 16 16">
+                              <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576zm6.787-8.201L1.591 6.602l4.339 2.76z"/>
+                            </svg>
+                            &nbsp;Send
+                          </span>
+                        </button>
+                        <button data-cursor="block" className='mt-5 ms-auto' type='reset'>
+                          <span>
+                          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
+                            <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
+                          </svg>
+                            &nbsp;Reset
+                          </span>
+                        </button>
+                      </div>
                     </form>
                 </div>
                 </Col>

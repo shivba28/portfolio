2175924c9c4069f90318c1f0de1bb71efa27a470
shivba28 <shivba28@gmail.com>
2025-03-18 09:24:47 -0700

    contact form working properly

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,32 +8,84 @@ import 'aos/dist/aos.css';
 import AOS from 'aos';
 
 export const Contact = () => {
-  useEffect(() => {
-    AOS.init();
-  }, [])
-    const form = useRef();
+  const form = useRef();
+  const userName = useRef(null);
+  const userEmail = useRef(null);
+  const userMessage = useRef(null);
+  
+
+  // <!-- emailjs to mail contact form data -->
+  emailjs.init("m6mPxttHF1Ya0wQ3-");
+
+        
+  const validateForm = () => {
+    const name = userName.current.value;
+    const email = userEmail.current.value;
+    const message = userMessage.current.value;
+              
+    // Validation functions
+          function validateEmail(email) {
+              const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+              return re.test(String(email).toLowerCase());
+          }
+
+          function validateName(name) {
+              return name.length >= 3; // Ensure the name has at least 3 characters
+          }
+
+          function validateMessage(message) {
+              return message.length >= 15; // Ensure the message has at least 15 characters
+          }
+
+          // Check validations
+          if (!validateName(name)) {
+              alert("Please enter your full name with at least 3 characters.");
+              return;
+          }
+
+          if (!validateEmail(email)) {
+              alert("Please enter a valid email address.");
+              return;
+          }
+
+          if (!validateMessage(message)) {
+              alert("Please enter a message with at least 15 characters.");
+              return;
+          }
+  }
 
     const sendEmail = (e) => {
       e.preventDefault();
-  
-      emailjs
-        .sendForm('service_x2gf7d5', 'template_826fsnl', form.current, {
-          publicKey: 'm6mPxttHF1Ya0wQ3-',
-        })
-        .then(
-          () => {
-            console.log('SUCCESS!');
-          },
-          (error) => {
-            console.log('FAILED...', error.text);
-          },
-        );
+      if(validateForm()) {
+        console.log('Validated')
+      }  else {
+        console.log('Validation Failed')
+      }
+
+        emailjs.send('service_x2gf7d5', 'template_826fsnl', {
+          to_name: "Shivba",
+          from_name: userName.current.value,
+          message: userMessage.current.value,
+          reply_to: userEmail.current.value
+      })
+          .then( (response) => {
+              console.log('SUCCESS!', response.status, response.text);
+              document.getElementById("contact-form").reset();
+              alert("Form Submitted Successfully");
+          }, (error) => {
+              console.log('FAILED...', error);
+              alert("Form Submission Failed! Try Again");
+          });
     };
 
+    useEffect(() => {
+      AOS.refreshHard();
+      AOS.init();
+    }, [])
 
     return(
         <section className="contact content" id='contact'>
-            <div className="title text-center d-flex flex-wrap pt-4 bg-gradient" data-aos="fade-down" data-aos-offset="500">
+            <div className="title text-center d-flex flex-wrap pt-4 bg-gradient" data-aos="fade-down">
                 <div className="slogan-left ms-auto"><h1 className="left">CONTACT</h1></div>
                 <div className="slogan-right me-auto"><h1 className="right">ME</h1></div>
             </div>
@@ -43,40 +95,40 @@ export const Contact = () => {
                 </Col>
                 <Col sm={12} md={12} lg={6} xl={6} className='d-grid mt-5 col'>
                 <div id="form-content" className='mx-auto rounded-4 bg-light bg-gradient'>
-                <header id="header" className="bg-black bg-gradient rounded-top-4">Let's Connect</header>
-                    <form className='form d-grid mx-5' ref={form} onSubmit={sendEmail}>
-                      <div className="form__group field">
-                        <span className="form__label">Name</span>
-                        <input data-cursor="text" className='form__field' type="text" name="user_name" placeholder='Name' id='name' autoComplete='on' />
-                      </div>
-                      <div className="form__group field">
-                        <span className="form__label">Email</span>
-                        <input data-cursor="text" className='form__field' type="email" name="user_email" placeholder='Email' id='email' autoComplete='on' />
-                      </div>
-                      <div className="form__group field">
-                        <span className="form__label">Message</span>
-                        <textarea data-cursor="text" className='form__field' name="user_message" placeholder='Message' id='message' />
-                      </div>
-                      <div className='d-flex flex-row flex-wrap'>
-                        <button data-cursor="block" data-cursor-style="background: transparent" className='mt-5 me-auto' type="submit">
-                          <span>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-send" viewBox="0 0 16 16">
-                              <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576zm6.787-8.201L1.591 6.602l4.339 2.76z"/>
-                            </svg>
-                            &nbsp;Send
-                          </span>
-                        </button>
-                        <button data-cursor="block" data-cursor-style="background: transparent" className='mt-5 ms-auto' type='reset'>
-                          <span>
-                          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash" viewBox="0 0 16 16">
-                            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
-                            <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
+                  <header id="header" className="bg-black bg-gradient rounded-top-4">Let's Connect</header>
+                  <form className='form d-grid mx-5' ref={form} id="contact-form" onSubmit={sendEmail}>
+                    <div className="form__group field">
+                      <span className="form__label">Name</span>
+                      <input data-cursor="text" ref={userName} className='form__field' type="text" name="user_name" placeholder='Name' id='name' />
+                    </div>
+                    <div className="form__group field">
+                      <span className="form__label">Email</span>
+                      <input data-cursor="text" ref={userEmail} className='form__field' type="email" name="user_email" placeholder='Email' id='email' />
+                    </div>
+                    <div className="form__group field">
+                      <span className="form__label">Message</span>
+                      <textarea data-cursor="text" ref={userMessage} className='form__field' name="message" placeholder='Message' id='message' />
+                    </div>
+                    <div className='d-flex flex-row flex-wrap'>
+                      <button data-cursor="block" data-cursor-style="background: transparent" className='mt-5 me-auto' type="submit">
+                        <span>
+                          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-send" viewBox="0 0 16 16">
+                            <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576zm6.787-8.201L1.591 6.602l4.339 2.76z"/>
                           </svg>
-                            &nbsp;Reset
-                          </span>
-                        </button>
-                      </div>
-                    </form>
+                          &nbsp;Send
+                        </span>
+                      </button>
+                      <button data-cursor="block" data-cursor-style="background: transparent" className='mt-5 ms-auto' type='reset'>
+                        <span>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash" viewBox="0 0 16 16">
+                          <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
+                          <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
+                        </svg>
+                          &nbsp;Reset
+                        </span>
+                      </button>
+                    </div>
+                  </form>
                 </div>
                 </Col>
             </Row>
